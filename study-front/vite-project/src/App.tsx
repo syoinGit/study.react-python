04655dc943930518b0daf74c26ef66fbccd63470
syoinGit <sasasyuga@yahoo.co.jp>
2026-01-08 21:33:01 +0900

    初回コミット

---
+++ study-front/vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div>
+      <h1>Hello World</h1>
+    </div>
+
+    <div>
+    <p>～とりあえずいい感じに文字打ってみた～</p>
+    </div>
+    </>
+      )
+}
+
+export default App
